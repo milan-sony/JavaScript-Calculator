3c8084cce97c519816b3c58b9300e62edb4ad6e5
Milan Sony <milanfrom2001@gmail.com>
2022-08-06 00:31:07 +0530

    Small tweaks made and comments added in files

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+//* Access HTML Elements
 let screen = document.getElementById("screen");
 
+//! Value is accessed from the input field and the btnClick is added as onclick event in HTML - Input field
+
+//* Add EventListener to button
 function btnClick(value) {
   screen.value += value;
 }
 
+//* Clean Screen
 function clearScreen() {
   screen.value = "";
 }
+
+//* To find result
 function findResult() {
   let result = eval(screen.value);
   screen.value = result;
